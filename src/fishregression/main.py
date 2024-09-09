35c11d81f120e33e638dc996e02b2c5fc8205e32
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-09 20:32:39 +0900

    fix

--- src/fishregression/main.py
+++ src/fishregression/main.py
@@ -13,7 +13,7 @@ app=FastAPI()
 def get_model_path():
     import os
     dir_name=os.path.dirname(__file__)
-    model_path=os.path.join(dir_name, "model/model1.pkl")
+    model_path=os.path.join(dir_name, "model1.pkl")
 
     return model_path
                       
