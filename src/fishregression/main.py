ca61e5253ad1a15096535b90fd7dd71200b4d5bd
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-09 20:38:59 +0900

    rm model

--- src/fishregression/main.py
+++ src/fishregression/main.py
@@ -14,6 +14,8 @@ def get_model_path():
     import os
     dir_name=os.path.dirname(__file__)
     model_path=os.path.join(dir_name, "model1.pkl")
+    
+    print(model_path)
 
     return model_path
                       
--- src/fishregression/model/manager.py
+++
@@ -1,6 +0,0 @@
-def get_model_path():
-    import os
-    dir_name=os.path.dirname(__file__)
-    model_path=os.path.join(dir_name, "model1.pkl")
-
-    return model_path
